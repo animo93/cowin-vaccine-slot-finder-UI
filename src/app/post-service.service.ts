c04f1a8adfeb273ca78ae249c9a62218e7d8642b
animo <animeshmohanty84@gmail.com>
2021-06-08 00:51:55 +0530

    Fixed Safari browser issue & postbuild for copying service worker

--- src/app/post-service.service.ts
+++ src/app/post-service.service.ts
@@ -13,7 +13,7 @@ export class PostServiceService {
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type':  'application/json',
-      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
+      //"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
     })
   };
 
